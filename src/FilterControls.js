b6dbbdb8bc37f936a298532f44c2b8540cd06d1c
Adam Niedzwiecki <adamn8142@gmail.com>
2019-02-23 16:23:44 -0700

    Add photos

--- src/FilterControls.js
+++ src/FilterControls.js
@@ -24,14 +24,15 @@ class FilterControls extends Component {
     }, this.filteredCategories)
   }
 
+ 
   render() {
     return (
       <div>
         <div className="controls-container">
           <button className="button" id="lvl-1-btn" value="8 kyu" onClick={this.setButtontToState}>Level 1</button>
           <button className="button" id="lvl-2-btn" value="7 kyu" onClick={this.setButtontToState}>Level 2</button>
-          <button className="button" id="lvl-3-btn"  value="6 kyu"onClick={this.setButtontToState}>Level 3</button>
-          <button className="button" id="completed-btn"  value="completed"onClick={this.setButtontToState}>Completed</button>
+          <button className="button" id="lvl-3-btn" value="6 kyu" onClick={this.setButtontToState}>Level 3</button>
+          <button className="button" id="completed-btn" value="completed" onClick={this.setButtontToState}>Completed</button>
         </div>
           <CardContainer data={this.props.data} filteredCategories={this.filteredCategories()} />
       </div>
