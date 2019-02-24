b77aa1d4aaad01c264b8b68cee2dcf09e7a27cab
Adam Niedzwiecki <adamn8142@gmail.com>
2019-02-23 18:40:47 -0700

    Local storage

--- src/FilterControls.js
+++ src/FilterControls.js
@@ -7,34 +7,48 @@ class FilterControls extends Component {
   constructor() {
     super();
     this.state = {
-      chosenCategory: ""
+      chosenCategory: "",
+      completed: []
     }
   }
 
   filteredCategories = () => {
     let filteredCats = this.props.data.filter((codeWar) => {
-      return codeWar.level === this.state.chosenCategory
+      if (codeWar.level === this.state.chosenCategory && !codeWar.addedToStorage) {
+        return codeWar
+      } else if (this.state.chosenCategory === 'completed' && codeWar.addedToStorage) {
+        return codeWar
+      }
     })
     return filteredCats
   }
+  
+    setButtonToState = (event) => {
+      this.setState({
+        chosenCategory: event.target.value
+      }, this.filteredCategories)
+    }
+  
+  // filterCompleted = () => {
+  //   let completedCodeWars = this.state.completed.map((codeWar) => {
+  //      codeWar.addedToStorage === true;
+  //   })  
+  //   return completedCodeWars 
+  // }
 
-  setButtontToState = (event) => {
-    this.setState({
-      chosenCategory: event.target.value
-    }, this.filteredCategories)
-  }
+
 
  
   render() {
     return (
       <div>
         <div className="controls-container">
-          <button className="button" id="lvl-1-btn" value="8 kyu" onClick={this.setButtontToState}>Level 1</button>
-          <button className="button" id="lvl-2-btn" value="7 kyu" onClick={this.setButtontToState}>Level 2</button>
-          <button className="button" id="lvl-3-btn" value="6 kyu" onClick={this.setButtontToState}>Level 3</button>
-          <button className="button" id="completed-btn" value="completed" onClick={this.setButtontToState}>Completed</button>
+          <button className="button" id="lvl-1-btn" value="8 kyu" onClick={this.setButtonToState}>Level 1</button>
+          <button className="button" id="lvl-2-btn" value="7 kyu" onClick={this.setButtonToState}>Level 2</button>
+          <button className="button" id="lvl-3-btn" value="6 kyu" onClick={this.setButtonToState}>Level 3</button>
+          <button className="button" id="completed-btn" value="completed" onClick={this.setButtonToState}>Completed</button>
         </div>
-          <CardContainer data={this.props.data} filteredCategories={this.filteredCategories()} />
+          <CardContainer toggleComplete={this.props.toggleComplete} data={this.props.data} filteredCategories={this.filteredCategories()} filteredCompleted={this.filterCompleted} />
       </div>
     )
   }
