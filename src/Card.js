4b7be49be0fc695970ca713d4493b523e03edea1
Adam Niedzwiecki <adamn8142@gmail.com>
2019-02-25 13:24:41 -0700

    Fetches data, removes dataSet state

--- src/Card.js
+++ src/Card.js
@@ -35,12 +35,12 @@ class Card extends Component {
           </div>
           <h4 className="card-prompt">{this.props.codeWar.prompt}</h4>
           <div className="card-buttons-container">
-            {!this.state.toggledSolution && <button className="card-button" className="see-solution-btn"onClick={this.toggleSolution}>See the Solution</button>}
-            {this.state.toggledSolution && <button className="card-button" className="see-solution-btn"onClick={this.toggleSolution}>Hide the Solution</button>}
             {!this.props.codeWar.addedToStorage && <button className="card-button" className="save-to-storage-btn" onClick={this.toggleCompletedProperty}>Mark as Completed!</button>}
             {this.props.codeWar.addedToStorage && <button className="card-button" className="save-to-storage-btn" onClick={this.toggleCompletedProperty}>Remove from Completed!</button>}
+            {!this.state.toggledSolution && <button className="card-button" className="see-solution-btn"onClick={this.toggleSolution}>See Solution</button>}
+            {this.state.toggledSolution && <button className="card-button" className="see-solution-btn"onClick={this.toggleSolution}>Hide Solution</button>}
           </div>
-            <img className="solution-image" src={`../CodewarSolutions/${this.props.codeWar.id}.png`} /> 
+            {this.state.toggledSolution && <img className="solution-image" src={`../CodewarSolutions/${this.props.codeWar.id}.png`} />}
         </article>
       </div>
     )
