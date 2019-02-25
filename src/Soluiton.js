84ebeeae258eedbb88f01ea5b95985e92aa1dda9
Adam Niedzwiecki <adamn8142@gmail.com>
2019-02-25 10:33:00 -0700

    Add Solution component

---
+++ src/Soluiton.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+class Solution extends Component {
+  constructor() {
+    super();
+  }
+
+
+
+
+
+  render(){
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Solution; 
