53a085e9f68c0ece2fafd5c4449c2557b5860bde
Adam Niedzwiecki <adamn8142@gmail.com>
2019-02-26 19:00:45 -0700

    App test

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -6,6 +6,38 @@ import { shallow } from "enzyme";
 describe("App", () => {
   let wrapper;
 
+  let mockData = {
+    data: [
+      {
+        "id": 1,
+        "name": "Abbreviate a Two Word Name",
+        "type": "String",
+        "prompt": "Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.",
+        "level": "8 kyu",
+        "replLink": "https://repl.it/@codesForDays/Abbreviate-a-Two-Word-Name?language=nodejs&folderId=",
+        "addedToStorage": false
+      },
+      {
+        "id": 2,
+        "name": "Welcome to the City",
+        "type": "String",
+        "prompt": "Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.",
+        "level": "8 kyu",
+        "replLink": "https://repl.it/@codesForDays/Welcome-to-the-City?language=nodejs&folderId=",
+        "addedToStorage": false
+      },
+        {
+        "id": 3,
+        "name": "Remove Anchor from URL",
+        "type": "String",
+        "prompt": "Complete the function/method so that it returns the url with anything after the anchor (#) removed.",
+        "level": "7 kyu",
+        "replLink": "https://repl.it/@codesForDays/Remove-anchor-from-URL?language=nodejs&folderId=",
+        "addedToStorage": false
+      }
+    ],
+  }
+
   beforeEach(()=> {
     wrapper = shallow(
       <App />
@@ -19,4 +51,41 @@ describe("App", () => {
   it("should match the snapshot with all data passed in", () => {
     expect(wrapper).toMatchSnapshot();
   })
+
+  it("should toggle the completed prop", () => {
+    let expected = [
+      {
+        "id": 1,
+        "name": "Abbreviate a Two Word Name",
+        "type": "String",
+        "prompt": "Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.",
+        "level": "8 kyu",
+        "replLink": "https://repl.it/@codesForDays/Abbreviate-a-Two-Word-Name?language=nodejs&folderId=",
+        "addedToStorage": false
+      },
+      {
+        "id": 2,
+        "name": "Welcome to the City",
+        "type": "String",
+        "prompt": "Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.",
+        "level": "8 kyu",
+        "replLink": "https://repl.it/@codesForDays/Welcome-to-the-City?language=nodejs&folderId=",
+        "addedToStorage": true
+      },
+      {
+        "id": 3,
+        "name": "Remove Anchor from URL",
+        "type": "String",
+        "prompt": "Complete the function/method so that it returns the url with anything after the anchor (#) removed.",
+        "level": "7 kyu",
+        "replLink": "https://repl.it/@codesForDays/Remove-anchor-from-URL?language=nodejs&folderId=",
+        "addedToStorage": false
+      }
+    ]
+    wrapper.setState({data:mockData.data})
+    wrapper.instance().toggleComplete(2)
+    expect(wrapper.state("data")).toEqual(expected)
+
+  })
+
 });
